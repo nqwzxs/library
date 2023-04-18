5ca0c878ae20547fd2e0fc6328661bb4285b5e21
nqwzxs <nqwzxs@gmail.com>
2023-04-18 16:08:15 +0500

    Add function which displays books

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const bookGrid = document.querySelector(".book-grid");
+
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -12,4 +14,24 @@ function Book(title, author, pages, isRead) {
 
 function addBookToLibrary(title, author, pages, isRead) {
     myLibrary.push(new Book(title, author, pages, isRead));
+}
+
+function displayBook() {
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        bookGrid.appendChild(bookCard); 
+        const bookTitle = document.createElement("div");
+        bookTitle.textContent = `Title: ${book.title}`;
+        bookCard.appendChild(bookTitle);
+        const bookAuthor = document.createElement("div");
+        bookAuthor.textContent = `Author: ${book.author}`;
+        bookCard.appendChild(bookAuthor);
+        const bookPages = document.createElement("div");
+        bookPages.textContent = `Number of pages: ${book.pages}`;
+        bookCard.appendChild(bookPages);
+        const bookStatus = document.createElement("div");
+        bookStatus.textContent = `Status: ${book.isRead ? "Read" : "Not Read"}`;
+        bookCard.appendChild(bookStatus);
+    });
 }
